6455411ac10834699d6d633363489c7ea057d6b3
Eli Goodfriend <eli.goodfriend@gmail.com>
2016-08-29 09:38:37 -0400

    bare minimum in place, but does it work?

--- module1/logisticRegression.py
+++ module1/logisticRegression.py
@@ -1,46 +1,83 @@
 import numpy as np
+from scipy import optimize
 from matplotlib import pyplot as plt
 
-def getWeights(x,t,N,M,K):
-  w = np.ones((M,K))
-  return w
+def getWeights(x, t, N, M, K, weightCost):
+  w0 = np.ones((M+1,K))
+  [weights, error, info]  = optimize.fmin_l_bfgs_b(logRegObjectiveOpt, w0, args=(t, x, weightCost, M, K), approx_grad=True) 
+  weights.shape = (M+1,K)
+  [w,b] = np.vsplit(weights, [M])
+  return w, b
 
-def predictClasses(x,w,N,M,K):
-  t = np.zeros((1,N))
+def logRegObjectiveOpt(wb, t, x, weightCost, M, K):
+  [w,b] = np.split(wb, [K*M])
+  w.shape = (M,K)
+  error = logRegObjective(w, b, t, x, weightCost)
+  return error
+
+def logRegObjective(w, b, t, x, weightCost):
+# TODO this is janky
+  [N,K] = t.shape
+  M = w.size / K
+  error = 0
+  for i in range(N):
+    y = predictClasses(x[i,:], w, b)
+    for j in range(K):
+      error += -t[i,j]*np.log(y[j])
+  for i in range(M):
+    for j in range(K):
+      error += weightCost*w[i,j]**2
+  return error
+
+def logRegGrad(w, b, t, x, weightCost):
   return t
 
+def predictClasses(x,w,b):
+  a = np.dot(x,w) + b
+  #TODO this is janky
+  if a.ndim > 1:
+    [N,K] = a.shape
+    yPred = np.zeros((N,K))
+    for n in range(N):
+      yPred[n,:] = softmax(a[n,:])
+  else:  
+    K = a.size
+    yPred = np.zeros((K))
+    yPred = softmax(a)
+  return yPred
+
 def generateData(N,M,K):
-  x1 = np.random.random(N)
-  x2 = np.random.random(N)
-  x = np.vstack((x1,x2))
-  
-  w = np.ones((M,K))
-  wT = w.transpose()
-  b = np.ones((K,N)) * -0.5
-  y = np.dot(wT,x) + b
-  t = np.zeros((1,N))
-  
-  # if y>0, then we're in class 1, otherwise in class 0
-  class1 = y>0
-  t += class1 * 1
+  x = np.random.random((N,M))
+  w = np.random.random((M,K))
+  b = np.ones((1,K)) * -0.5
 
-  ## write it out 
-  #writeData = np.vstack((x,t))
-  #np.savetxt('synData',writeData)
+  y = predictClasses(x,w,b)
+  t = np.zeros((N,K))
+  for rowIdx in range(N):
+    maxIdx = np.argmax(y[rowIdx,:])
+    t[rowIdx,maxIdx] = 1
 
-  plotData(x,t)
-
+  plotData(x,t,K)
   return x, t
 
-def plotData(x,t):
+def plotData(x,t,K):
+  [x1, x2] = np.hsplit(x, [1])
+  label = np.dot(t, np.arange(K))
   
-  #dataIn = np.loadtxt('synData')
-  
-  #[x, t] = np.vsplit(dataIn, [M])
-  [x1, x2] = np.vsplit(x, [1])
-  
-  plt.scatter(x1, x2, c=t, alpha=0.5, s=75)
+  plt.scatter(x1, x2, c=label, alpha=0.5, s=75)
   plt.show()
 
 def calcError(tPred,tReal):
-  return 0
+  error = (tPred - tReal)**2
+  return np.sum(error)
+
+def softmax(a):
+  # TODO this are numerically badz
+  K = a.size
+  softmax = np.zeros((K))
+  denom = 0
+  for colIdx in range(K):
+    denom += np.exp(a[colIdx])
+    softmax[colIdx] = np.exp(a[colIdx])
+  softmax /= denom 
+  return softmax
--- module1/runLogisticRegression.py
+++ module1/runLogisticRegression.py
@@ -6,19 +6,20 @@ import logisticRegression as lr
 def main():
   N = 15 # number of training data points
   M = 2  # dimension of data
-  K = 1  # number of classes - 1
+  K = 3  # number of classes
+  weightCost = 0.5 # punishment for large weights
 
   # get the training data, either generated or real
   [x,t] = lr.generateData(N,M,K)
 
   # train a model to classify the data
-  w = lr.getWeights(x,t,N,M,K)
+  [w,b] = lr.getWeights(x,t,N,M,K,weightCost)
 
   # get new data
   [xNew,tNew] = lr.generateData(N,M,K)
 
   # classify the new data using the model
-  tPred = lr.predictClasses(xNew,w,N,M,K)
+  tPred = lr.predictClasses(xNew,w,b)
 
   # how right were we?
   error = lr.calcError(tPred,tNew)
